04daac7904271e9f7541d567be54faefd9963f13
Yash Indane <yashindane46@gmail.com>
2021-03-03 15:44:41 +0530

    fourth commit

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-   return "hey this is version 2!!"
+   return "hey this is version 2!!"</b>  <img src='https://simplestorage1.blob.core.windows.net/con1/img.png'>
